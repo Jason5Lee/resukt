f5371d63e4a9a296034eb4624d11c5e2f8441f19
Jason5Lee <jason5lee@hotmail.com>
2021-08-09 18:28:12 +0800

    fix CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -165,17 +165,5 @@ kotlin {
         val iosArm64Main by getting
 
     }
-    
-//    val hostOs = System.getProperty("os.name")
-//    val isMingwX64 = hostOs.startsWith("Windows")
-//    val nativeTarget = when {
-//        hostOs == "Mac OS X" -> macosX64("native")
-//        hostOs == "Linux" -> linuxX64("native")
-//        isMingwX64 -> mingwX64("native")
-//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-//    }
-//
-//
-
 }
 
